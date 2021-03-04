46217062b38fea510652fcf493c44d43c16976fa
KFischer (MBP) <klaus.fischer@eloware.com>
2021-03-04 20:57:40 +0100

    Initial Commit

---
+++ playintSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  playintSwiftUi
+//
+//  Created by Klaus Fischer on 04.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ playintSwiftUi/playintSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  playintSwiftUiApp.swift
+//  playintSwiftUi
+//
+//  Created by Klaus Fischer on 04.03.21.
+//
+
+import SwiftUI
+
+@main
+struct playintSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
