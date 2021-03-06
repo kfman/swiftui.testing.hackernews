bdb9599f0f9721e7bf3b2db0fab3efb006b1dde9
KFischer (MBP) <klaus.fischer@eloware.com>
2021-03-06 20:36:49 +0100

    String Int confusion

--- playintSwiftUi/Services/RestBaseService.swift
+++ playintSwiftUi/Services/RestBaseService.swift
@@ -7,21 +7,38 @@
 
 import Foundation
 
+struct Settings{
+    static let baseUrl : String = "https://hacker-news.firebaseio.com/v0"
+}
 
-class RestBaseService<TModel> where TModel: Codable{
-    
-    func get(url: URL, finished: (Any)-> Void ){
+class RestBaseService{
+
+
+    func get<TResult>(endpoint: String, finished: @escaping (TResult) -> Void) where TResult: Codable{
+        let completeUrl = "\(Settings.baseUrl)\(endpoint)"
+        guard let callUrl = URL(string: completeUrl) else {
+            print("Error on creating url \(completeUrl)")
+            return
+        }
         
-        URLSession.init(configuration: .default).dataTask(with: url, completionHandler: { (data, response, error) in
+        URLSession.init(configuration: .default).dataTask(with: callUrl, completionHandler: { (data, response, error) in
             if let data = data {
-                if let result = try? JSONDecoder().decode(TModel.self, from: data){
+                if let result = try? JSONDecoder().decode(TResult.self, from: data){
                     print("Got what...\(result)")
+                    finished(result)
+                }else{
+                    print("Error on decoding \(TResult.self) result \(String(bytes: data, encoding: .utf8) ?? "Bad error")")
                 }
             }
+            
+            if let error = error {
+                    print(error)
+            }
+            
         }).resume()
         
+
     }
-    
 }
 
 struct HackerNews : Codable{
@@ -30,19 +47,21 @@ struct HackerNews : Codable{
     let headline: String
 }
 
-class NewsCollection:RestBaseService<[String]>{
+class NewsCollection:RestBaseService{
     
-    private let url: String
+    private let endpoint: String
     
-    init(url: String){
-        self.url = url
+    init(endpoint: String){
+        self.endpoint = endpoint
     }
     
-    func getTopStoryIds(onLoaded: @escaping ([String]) -> Void){
-        
+    func getTopStoryIds(onLoaded: @escaping ([Int]) -> Void){
+        super.get(endpoint: endpoint) { (data: [Int]) in
+            onLoaded(data)
+        }
     }
     
-    func getStory(id: String, onLoaded: @escaping (HackerNews) -> Void ){
+    func getStory(id: Int, onLoaded: @escaping (HackerNews) -> Void ){
         
     }
 }
@@ -51,21 +70,40 @@ class NewsCollection:RestBaseService<[String]>{
 
 
 class HackerNewsService {
+    let newsCollection: NewsCollection
+    
+    
+    init() {
+        newsCollection = NewsCollection(endpoint: "/topstories.json")
+    }
     
     func getNews( _ finished: @escaping ([HackerNews])->Void){
         var news = [HackerNews]()
-        news.append(HackerNews(id: 0, url: URL(string: "http://www.google.de")!, headline: "Test #1"))
-        news.append(HackerNews(id: 1, url: URL(string: "http://www.google.de")!, headline: "Test #2"))
-        news.append(HackerNews(id: 2, url: URL(string: "http://www.google.de")!, headline: "Test #3"))
-        news.append(HackerNews(id: 3, url: URL(string: "http://www.google.de")!, headline: "Test #4"))
-        news.append(HackerNews(id: 4, url: URL(string: "http://www.google.de")!, headline: "Test #5"))
-        news.append(HackerNews(id: 5, url: URL(string: "http://www.google.de")!, headline: "Test #6"))
-        news.append(HackerNews(id: 6, url: URL(string: "http://www.google.de")!, headline: "Test #7"))
-        news.append(HackerNews(id: 7, url: URL(string: "http://www.google.de")!, headline: "Test #8"))
-        news.append(HackerNews(id: 8, url: URL(string: "http://www.google.de")!, headline: "Test #9"))
-        news.append(HackerNews(id: 9, url: URL(string: "http://www.google.de")!, headline: "Test #10"))
+//        news.append(HackerNews(id: 0, url: URL(string: "http://www.google.de")!, headline: "Test #1"))
+//        news.append(HackerNews(id: 1, url: URL(string: "http://www.google.de")!, headline: "Test #2"))
+//        news.append(HackerNews(id: 2, url: URL(string: "http://www.google.de")!, headline: "Test #3"))
+//        news.append(HackerNews(id: 3, url: URL(string: "http://www.google.de")!, headline: "Test #4"))
+//        news.append(HackerNews(id: 4, url: URL(string: "http://www.google.de")!, headline: "Test #5"))
+//        news.append(HackerNews(id: 5, url: URL(string: "http://www.google.de")!, headline: "Test #6"))
+//        news.append(HackerNews(id: 6, url: URL(string: "http://www.google.de")!, headline: "Test #7"))
+//        news.append(HackerNews(id: 7, url: URL(string: "http://www.google.de")!, headline: "Test #8"))
+//        news.append(HackerNews(id: 8, url: URL(string: "http://www.google.de")!, headline: "Test #9"))
+//        news.append(HackerNews(id: 9, url: URL(string: "http://www.google.de")!, headline: "Test #10"))
         
-        finished(news)
+        let group = DispatchGroup()
+        newsCollection.getTopStoryIds { (ids) in
+            for id in ids {
+                group.enter()
+                self.newsCollection.getStory(id: id) { (item) in
+                    news.append(item)
+                    group.leave()
+                }
+            }
+        }
+        
+        group.notify(queue: .main, execute: {
+            finished(news)
+        })
     }
     
 }
